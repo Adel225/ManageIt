cbcd0a25d46670159bf44e60329eef865d6a8886
Adel <adel.samehlabib@gmail.com>
2026-08-05 12:04:58 +0300

    fonts added

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -10,19 +10,13 @@ export default function App() {
   return (
     <SafeAreaView className="flex-1 bg-background p-5">
       <StatusBar barStyle="dark-content" /> 
-      <Text className="text-xl font-bold text-success">
-        Welcome to Nativewind!
-      </Text>
-      <Link href="/onboarding" className="mt-4 rounded bg-primary text-white p-4">Go to OnBoarding</Link>
-      <Link href="/(auth)/sign-in" className="mt-4 rounded bg-primary text-white p-4">Go to sign in</Link>
-      <Link href="/(auth)/sign-up" className="mt-4 rounded bg-primary text-white p-4">Go to sign up</Link>
-      <Link href="/subscriptions/spotify" className="mt-4 rounded bg-primary text-white p-4">Spotify subscription</Link>
-      <Link href={{
-        pathname: "/subscriptions/[id]",
-        params: { id: "claude" }
-      }} className="mt-4 rounded bg-primary text-white p-4">
-        Claude Max Subscription
-      </Link>
+
+      <Text className="text-5xl font-sans-extrabold">Home</Text>
+
+      <Link href="/onboarding" className="mt-4 font-sans-bold rounded bg-primary text-white p-4">Go to OnBoarding</Link>
+      <Link href="/(auth)/sign-in" className="mt-4 font-sans-bold rounded bg-primary text-white p-4">Go to sign in</Link>
+      <Link href="/(auth)/sign-up" className="mt-4 font-sans-bold rounded bg-primary text-white p-4">Go to sign up</Link>
+      
     </SafeAreaView>
   );
 }
